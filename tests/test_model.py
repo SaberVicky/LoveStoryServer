9cbfd41c1d39dcc6c5cbcc77a1c181df73f84ff2
guochunyang <gcy@zhihu.com>
2017-01-07 16:39:30 +0800

    add tests

---
+++ tests/test_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author: Chunyang Guo
+#
+# Copyright (C) 2016 Zhihu Inc.
+
+from __future__ import absolute_import
+
+from unittest import TestCase
+
+
+class TestUserModel(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_user(self):
+        self.assertEqual(1, 1)
+        pass
